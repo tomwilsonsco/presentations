d5cde2e48b6092419fbc05aa2e7e1eae635a2dda
tomwilsonsco <thomaswilson81@gmail.com>
2024-01-22 09:24:34 +0000

    standards

---
+++ coding_standards/my_test.R
@@ -0,0 +1,7 @@
+my_vector <- c(10, 20, 30)
+
+my_calc <- function(x, multiplier) {
+  x * multiplier
+}
+
+my_calc(my_vector, multiplier=10)
---
+++ coding_standards/reformat_code.R
@@ -0,0 +1,5 @@
+myVector=c(10,20,30)
+
+myCalc=function(x,Multiplier){x*Multiplier}
+
+myCalc(myVector,Multiplier=10)
